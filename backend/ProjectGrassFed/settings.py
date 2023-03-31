d798ebe9fa905a8170fb062792f03e2ffcb94991
TheHolySock <75141747+TheHolySock@users.noreply.github.com>
2023-03-31 15:22:29 -0500

    Update settings.py
    For Melvin

--- backend/ProjectGrassFed/settings.py
+++ backend/ProjectGrassFed/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'ProjectGrassFed',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ProjectGrassFed.urls'
@@ -75,8 +83,12 @@ WSGI_APPLICATION = 'ProjectGrassFed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('mydatabase'),
+        'USER': os.environ.get('postgres'),
+        'PASSWORD': os.environ.get('postgres'),
+        'HOST': os.environ.get('db'),
+        'PORT': os.environ.get('5432'),
     }
 }
 
